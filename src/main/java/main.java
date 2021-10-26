d63ded9b897a8e628be9f2b066fc1ea83a0a0d91
cah-jamiegingins <64488206+cah-jamiegingins@users.noreply.github.com>
2021-10-26 15:48:30 -0400

    Print width working with name and craft

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -26,23 +26,6 @@ public class main {
         }
         System.out.print("|");
     }
-    private static int findWidestName(Astronauts astronauts) {
-    //I wanted to do a binary sort but I was taking too long and just did bubble sort
-
-        int biggestNameLength = 0;
-        String curName;
-        int curNameLength;
-
-        for(int num = 0; num < astronauts.getPeople().size(); num++)
-        {
-            curName = astronauts.getPersonsName(num);
-            curNameLength = curName.length();
-            if (curNameLength > biggestNameLength) {
-                biggestNameLength = curNameLength;
-            }
-        }
-        return biggestNameLength;
-    }
     private static int findWidest(Astronauts astronauts, String field) {
 
         int biggestLength = 0;
